58ef6923ea114085184c005db69ee37573a9c7b1
Troy Moench <brewersarenumber1@gmail.com>
2023-04-05 08:53:14 -0500

    Tweaked example 3 a little

--- example3.py
+++ example3.py
@@ -1,11 +1,8 @@
 # example 3: Anti-patterns/unexpected behavior
 from datetime import *
 
+print("The day is", date.today())
+
 
 def foo(arg=[]):
     print(arg)
-
-try:
-    foo()
-except:
-    print("caught exception!")
